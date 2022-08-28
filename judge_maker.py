71e1f70591fa59214e2438608e5da04588fb0a4d
FujiiNoritsugu <quercus491@gmail.com>
2022-08-29 06:31:13 +0900

    レビュー指摘事項反映

--- judge_maker.py
+++ judge_maker.py
@@ -4,6 +4,13 @@ def make_judge(grade, points):
     Args:
         rank (_type_): _description_
     """
+    # gradeがA～Eの文字以外であった場合、例外を発生させる
+    if grade not in ('A', 'B', 'C', 'D', 'E'):
+        raise Exception('gradeがA～Eの文字でありません')
+    # pointsが0～100の数値の10個のリストでなかった場合、例外を発生させる
+    if not (isinstance(points, list) and len(list) == 10 and all(
+            [isinstance(a, int) and 0 <= a <= 100 for a in points])):
+        raise Exception('pointsが整数値0～100のリストでありません')
     result = None
     # 点数に10点より下の点数が一つでもあるか判定
     is_failure = any((data < 10 for data in points))
@@ -11,14 +18,10 @@ def make_judge(grade, points):
     is_retest = True if sum((data <= 30 for data in points)) >= 3 else False
 
     if is_failure or grade == 'E':
-        result = '不合格'
+        result = 3
     elif is_retest or grade == 'D':
-        result = '再テスト'
+        result = 2
     elif grade in ('A', 'B', 'C'):
-        result = '合格'
+        result = 1
 
     return result
-
-
-if __name__ == '__main__':
-    make_judge('A', [1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
